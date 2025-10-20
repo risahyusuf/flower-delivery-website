d27e6f93e055a0d1fbabaa4b096a88cb8dfa8f36
risahyusuf <risahyusuf@gmail.com>
2025-10-20 16:37:03 +0100

    upload fix

--- backend/middleware/flowerUpload.js
+++ backend/middleware/flowerUpload.js
@@ -1,6 +1,12 @@
 const multer = require('multer');
 const path = require('path');
 
+const fs = require('fs');
+const uploadPath = path.join(__dirname, '..', 'uploads');
+if (!fs.existsSync(uploadPath)) {
+  fs.mkdirSync(uploadPath);
+}
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, 'uploads/');
