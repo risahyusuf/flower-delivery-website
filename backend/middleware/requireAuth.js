aa6381bf34f9405b9539202ce2a84d5da0b4c635
risahyusuf <risahyusuf@gmail.com>
2025-10-31 20:33:02 +0100

    id to Id

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -8,8 +8,8 @@ const requireAuth = async (req, res, next) => {
   const token = authorization.split(" ")[1];
 
   try {
-    const { id } = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = await User.findOne({ _id }).select('id')
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = await User.findById({ userId }).select('_id')
     next();
   } catch (error) {
     res.status(401).json({ message: "Request not authorized" });
